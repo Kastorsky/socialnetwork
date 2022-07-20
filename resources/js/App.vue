f91751e60de9a831b48d9e48ad35a8dea6320bc1
Evgeny Sudbin <kastorsky@mail.ru>
2022-07-20 23:18:03 +0300

    Full settings (Laravel, Passport, Vite, Vue3)

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello I`am from Vue</h1>
+    <h2>Hello I`am from Vue</h2>
+    <h3>Hello I`am from Vue</h3>
+    <h4>Hello I`am from Vue</h4>
+    <h5>Hello I`am from Vue</h5>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
